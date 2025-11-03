609bc96f7c6f2f233dc7129cc829bbedb7d0c5f9
Amir zekry <amirzekry43@gmail.com>
2025-11-03 11:02:52 +0200

    'text-fix'

--- app/(main)/item/[id]/Hero.jsx
+++ app/(main)/item/[id]/Hero.jsx
@@ -10,7 +10,7 @@ async function Hero({ id }) {
         <div className="flex flex-col md:flex-row items-center justify-center gap-8 md:gap-12 px-6 py-12 md:h-screen h-screen mb-0">
             <div className="flex flex-col items-center md:items-start text-center md:text-left space-y-4">
                 <h1 className="text-2xl md:text-4xl font-bold">{item.name}</h1>
-                <p className="max-w-xs md:max-w-md text-sm md:text-base">{item.discription}</p>
+                <p className="max-w-xs md:max-w-md text-sm md:text-base text-right">{item.discription}</p>
                 <Button className="w-full md:w-auto px-6 py-2 rounded-lg text-white font-semibold tracking-wide hover:cursor-pointer carbon-fiber shadow-md shadow-red-500 hover:scale-90 hover:shadow-lg">
                     <Link href='#features'>اعرف اكتر</Link>
                 </Button>
