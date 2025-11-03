4a3007f9e35f7ba5b2c4120500238c20950d9969
Amir zekry <amirzekry43@gmail.com>
2025-11-03 11:43:10 +0200

    'readyv3'

---
+++ prisma/migrations/20251103094254_init/migration.sql
@@ -0,0 +1,73 @@
+-- CreateTable
+CREATE TABLE "customer" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "number" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "governorate" TEXT NOT NULL,
+
+    CONSTRAINT "customer_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "feature" (
+    "id" TEXT NOT NULL,
+    "h1" TEXT NOT NULL,
+    "p" TEXT NOT NULL,
+    "itemId" TEXT,
+    "image_url" TEXT NOT NULL,
+
+    CONSTRAINT "feature_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "imageGallery" (
+    "id" TEXT NOT NULL,
+    "image_url" TEXT NOT NULL,
+    "itemId" TEXT,
+
+    CONSTRAINT "imageGallery_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "item" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "profit" DOUBLE PRECISION NOT NULL,
+    "image" TEXT NOT NULL,
+    "heroImage" TEXT NOT NULL,
+    "discription" TEXT NOT NULL,
+
+    CONSTRAINT "item_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "order" (
+    "id" TEXT NOT NULL,
+    "status" TEXT NOT NULL DEFAULT 'pending',
+    "total" DOUBLE PRECISION NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "customerId" TEXT,
+    "itemId" TEXT,
+    "notes" TEXT,
+
+    CONSTRAINT "order_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "customer_number_key" ON "customer"("number");
+
+-- AddForeignKey
+ALTER TABLE "feature" ADD CONSTRAINT "feature_itemId_fkey" FOREIGN KEY ("itemId") REFERENCES "item"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "imageGallery" ADD CONSTRAINT "imageGallery_itemId_fkey" FOREIGN KEY ("itemId") REFERENCES "item"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "order" ADD CONSTRAINT "order_customerId_fkey" FOREIGN KEY ("customerId") REFERENCES "customer"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "order" ADD CONSTRAINT "order_itemId_fkey" FOREIGN KEY ("itemId") REFERENCES "item"("id") ON DELETE SET NULL ON UPDATE CASCADE;
